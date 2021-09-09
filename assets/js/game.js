ca628224fb7903ef7f0ea379a98f91e779fa775f
Lesly <leslyr562@gmail.com>
2021-09-09 00:37:20 -0500

    added window promt

---
+++ assets/js/game.js
@@ -0,0 +1,15 @@
+var playerName = window.prompt("What is your robot name?");
+// What is this?
+console.log(playerName);
+console.log("This logs a string, good for leaving yourself a message");
+// this will od math and log 20
+console.log(10 + 10);
+// What is this?
+console.log ("Our robot's name is" + playerName);
+// Note the lack of quotation marks around playerName
+window.alert(playerName);
+
+function fight() {
+    window.alert("The fight has begun!");
+}
+// fight();
