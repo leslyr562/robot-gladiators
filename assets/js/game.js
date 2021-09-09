ec3f76f7010bb7fd5840a104fcfb992117a090f6
Lesly <leslyr562@gmail.com>
2021-09-09 01:43:46 -0500

    starting game

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,15 +1,24 @@
 var playerName = window.prompt("What is your robot name?");
-// What is this?
-console.log(playerName);
-console.log("This logs a string, good for leaving yourself a message");
-// this will od math and log 20
-console.log(10 + 10);
-// What is this?
-console.log ("Our robot's name is" + playerName);
-// Note the lack of quotation marks around playerName
-window.alert(playerName);
+var playerHealth = 100;
+var playerAttack = 10;
 
-function fight() {
-    window.alert("The fight has begun!");
-}
-// fight();
+var enemyName= "Roborto";
+var enemyHealth= 50;
+var enemyAttack = 12;
+
+var fight = function() {
+    window.alert("Welcome to Robot Gladiators!");
+};
+//fight();
+
+//Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
+enermy Health = enemyHealth - playerAttack;
+
+  // Log a resulting message to the console so we know that it worked.
+console.log (
+    playerName = "attacked" + enemyName + "now has" + enemyHealth "health remaininfing."
+);
+  // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
+
+  // Log a resulting message to the console so we know that it worked.
+};
